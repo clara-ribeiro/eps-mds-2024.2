2df98187f0dd19bfb9d7a8e729c86ea8cd34a7f4
Clara Ribeiro <clara.marcelino@descomplica.com.br>
2024-10-23 10:26:08 -0300

    feat: cria home

--- sentinelas-membros/src/App.js
+++ sentinelas-membros/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
 import ClaraRibeiro from './pages/ClaraRibeiro';
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/clara" element={<ClaraRibeiro/>}/>
       </Routes>
     </Router>
---
+++ sentinelas-membros/src/pages/Home/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './index.css';
+
+const alunos = [
+  { nome: "Clara Ribeiro", href: "/clara", turma: "EPS" },
+  { nome: "Danny Rodrigo", href: "/danny", turma: "MDS" },
+  { nome: "Eduardo Levenhagem", href: "/eduardo", turma: "MDS" },
+  { nome: "Guilherme Moura", href: "/guilherme", turma: "MDS" },
+  { nome: "Ígor Veras", href: "/igor", turma: "MDS" },
+  { nome: "João Igor", href: "/joao", turma: "MDS" },
+  { nome: "Jorge Henrique", href: "/jorge", turma: "MDS" },
+  { nome: "Marcos Antonio", href: "/marcos", turma: "MDS" },
+  { nome: "Mariana Martins", href: "/mariana", turma: "MDS" },
+  { nome: "Rodrigo Amaral", href: "/rodrigo", turma: "MDS" },
+  { nome: "Yzabella Pimenta", href: "/yzabella", turma: "MDS" }
+];
+
+const Home = () => (
+    <div>
+      <h1 className='titulo'>Projeto Sentinelas 2024.2</h1>
+      <div className='alunos'>
+        {alunos.map((pessoa, index) => (
+          <a key={index} className="botao" href={pessoa.href}>
+            <p className="nome">{pessoa.nome}</p>
+            <span>{pessoa.turma}</span>
+          </a>
+        ))}
+      </div>
+    </div>
+);
+
+export default Home;
